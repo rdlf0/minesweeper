a7671f796d67c53a9cfa0c5e084abfd15d0eda5b
Radoslav Stoyanov <r.stoyanov@outlook.com>
2020-05-14 04:54:00 -0400

    Reverted more import changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { Config, FIRST_CLICK, MODE_NAME } from "./config";
-import { BinaryToBase64UrlEncoderV2 } from "./encoder/binaryToBase64UrlEncoderV2";
 import { Game } from "./game";
+import { FIRST_CLICK, MODE_NAME, Config } from "./config";
 import { CantorPairer } from "./pairer/cantorPairer";
+import { BinaryToBase64UrlEncoderV2 } from "./encoder/binaryToBase64UrlEncoderV2";
 
 const config: Config = {
     mode: MODE_NAME.Expert,
